446211c0c3765a5e33b7e1c91e6d1aa127bc5788
rushabh101 <rushah101@gmail.com>
2021-04-09 02:42:06 +0530

    pid and png

--- ballbeam1.py
+++ ballbeam1.py
@@ -1,5 +1,6 @@
 import gym
 import ballbeam_gym
+import matplotlib.pyplot as plt
 
 # pass env arguments as kwargs
 kwargs = {'timestep': 0.05, 
@@ -12,18 +13,39 @@ kwargs = {'timestep': 0.05,
 env = gym.make('BallBeamSetpoint-v0', **kwargs)
 
 # constants for PID calculation
-Kp = 2.0
+Kp = 3.0
 Kd = 1.0
+Ki = 0.0
+
+i = 0
+
+ballPosn = []
+time = []
+beamAngle = []
 
 # simulate 1000 steps
-for i in range(100):   
+for t in range(100):
     # control theta with a PID controller
     env.render()
-    theta = Kp*(env.bb.x - env.setpoint) + Kd*(env.bb.v)
+    p = env.bb.x - env.setpoint
+    i = min(i + p/100, 0.005) if (i + p/100) >= 0 else max(i + p/100, -0.005)
+    d = env.bb.v
+    theta = Kp*p + Ki*i + Kd*d
     obs, reward, done, info = env.step(theta)
 
+    ballPosn.append(env.bb.x)
+    time.append(t)
+    beamAngle.append(env.bb.theta)
+
     if done:
-        print("Episode finished after {} timesteps".format(i+1))
+        print("Episode finished after {} timesteps".format(t+1))
         break
 
+fig1, ax1 = plt.subplots( nrows=1, ncols=1 )
+ax1.plot(time, ballPosn)
+fig1.savefig('1.png')
+
+fig2, ax2 = plt.subplots( nrows=1, ncols=1 )
+ax2.plot(time, beamAngle)
+fig2.savefig('2.png') 
 env.close()
